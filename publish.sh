ff13490e695bfc30af53a0f0947c450c30f5f51f
Bingzhi.Li <280761575@qq.com>
2024-07-24 23:33:53 +0800

    Sync

--- publish.sh
+++ publish.sh
@@ -5,5 +5,6 @@ rm -rf ./public
 hugo --theme=typo --baseURL="https://www.libingzhi.top/" --buildDrafts
 tar -cvf blog.tar ./public/
 scp blog.tar simon@119.23.255.199:/home/simon
+ssh simon@119.23.255.199 "sh /home/simon/blog.sh"
 rm ./blog.tar
 rm -rf ./public
